069685db0fb4488c63ac8a405fc14a63e53c4012
SigayAleksey <sigayaleksey@gmail.com>
2023-04-27 18:48:56 +0200

    first commit

---
+++ Felix/AppDelegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Felix
+//
+//  Created by Alexey Sigay on 27.04.2023.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+}
---
+++ Felix/AppDelegate/FelixApp.swift
@@ -0,0 +1,19 @@
+//
+//  FelixApp.swift
+//  Felix
+//
+//  Created by Alexey Sigay on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FelixApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
